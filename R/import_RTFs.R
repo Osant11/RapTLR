249778b76ff9028460029bc0f225b514cee0c983
Antoine Stos <astos1@its.jnj.com>
2026-02-12 05:20:16 -0500

    import_RTFs(): Function added to create a copy of rtf ouputs within the current working directory

---
+++ R/import_RTFs.R
@@ -0,0 +1,28 @@
+#' Create a copy of RTFs outputs within the current environment
+#'
+#' This function copy the dummy RTF outputs from the package and create a folder called "rtf_outputs" in the current working directory
+#'
+#' @examples
+#' # Just run the import_RTFs() function to create a copy folder within the RTFs outputs in your current working environment
+#' RapTLR::import_RTFs( )
+#' 
+#' @importFrom utils packageName
+#' @export
+import_RTFs <- function( ) {
+
+  src <- system.file(
+   "extdata/rtf_outputs",
+   package = utils::packageName( )
+ )
+  
+ if ( src == " " ) {
+   stop( "rtf_outputs not found in inst/extdata" )
+ }
+  
+ dest <- file.path( getwd() )
+ file.copy( from = src, to = dest, overwrite = TRUE, recursive = TRUE )
+ 
+ message( "[INFO] rtf outputs has been successfully imported and copied under: ", dest )
+  
+ invisible( dest )
+}
